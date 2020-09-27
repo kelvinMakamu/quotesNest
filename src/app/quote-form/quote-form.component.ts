3fc4aa04dd1f205b73dd39b098d1c9b0c0be67e5
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-09-27 15:45:42 +0300

    Adds quote-form component, completes Quote UI Design

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  
+  model = new Quote('1432VCS45', 'The quick brown fox','Beryl Nyange',0,0,'', new Date());
+
+  createNewQuote(){
+    alert("Hooray");
+  }
 
   constructor() { }
 
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
 
-    constructor(public id:string,public quote:string,public author:string,public upvotes:number,
-        public downvotes:number,public created_at:Date,public submitted_by:string){
+    constructor(public id: string, public quote: string, public author: string, public upvotes: number,
+        public downvotes: number, public submitted_by: string, public created_at: Date){
 
     }
 }
