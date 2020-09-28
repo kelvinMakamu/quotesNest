ad1719662aaf2a0c2049632029e7fc0420034d60
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-09-28 17:35:52 +0300

    Modifies Quote Model class to include showDetails boolean field

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
-
+    showQuoteDetails: boolean;
     constructor(public id: string, public quote: string, public author: string, public upvotes: number,
         public downvotes: number, public submitted_by: string, public created_at: Date){
-
+            this.showQuoteDetails=false;
     }
 }
